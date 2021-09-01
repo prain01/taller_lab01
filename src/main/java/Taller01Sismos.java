d8815aacd692abe999976e409bc3347def81d569
prain01 <71102083+prain01@users.noreply.github.com>
2021-09-01 10:54:24 -0400

    taller terminado, falta arreglar visualizacion(salida de datos mas bonita)

--- src/main/java/Taller01Sismos.java
+++ src/main/java/Taller01Sismos.java
@@ -7,23 +7,17 @@ public class Taller01Sismos {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         menu();
     }
 
-    public static void menu() {
+    public static void menu() throws IOException {
         System.out.println("[1] Ingresar datos\n" + //listo
                 "[2] Mostrar sismo de mayor magnitud\n" +//listo
                 "[3] Contar sismos (mayores/igual a 5.0)\n" +//listo
                 "[4] Enviar SMS (mayores/igual a 7.0)\n" +//listo
                 "[5] Salir\n");
-
-        //System.out.println("opcion: "+elegirOpcion());
-        double []datos  = ingresarSismos(crearArreglo());
-        mostrarSismos(datos);
-        mostrarDouble(buscarMayorSismo(datos));
-        mostrarInt(contarSismos(datos,5.0));
-        enviarSMS(contarSismos(datos,7.0));
+        opcionesMenu(elegirOpcion());
     }
 
     public static int elegirOpcion() throws IOException {
@@ -53,32 +47,57 @@ public class Taller01Sismos {
         }
     }
 
-    public static void opcionesMenu(int opcion){
-        double[] sismos;
-        switch (opcion){
-            case 1://Ingresar datos
-                sismos  = ingresarSismos(crearArreglo());
-                break;
-            case 2://Mostrar sismo de mayor magnitud
-                if (){
-
-                }
-                break;
-            case 3://Contar sismos (mayores/igual a 5.0)
-                break;
-            case 4://Enviar SMS (mayores/igual a 7.0)
-                break;
-            case 5://Salir
-                break;
+    public static void opcionesMenu(int opcion) throws IOException {
+        double[] sismos = crearArreglo();
+        while (opcion != 5){
+            switch (opcion){
+                case 1://Ingresar datos
+                    ingresarSismos(sismos);
+                    mostrarSismos(sismos);
+                    break;
+                case 2://Mostrar sismo de mayor magnitud
+                    caso2(sismos);
+                    break;
+                case 3://Contar sismos (mayores/igual a 5.0)
+                    caso3(sismos);
+                    break;
+                case 4://Enviar SMS (mayores/igual a 7.0)
+                    caso4(sismos);
+                    break;
+                case 5://Salir
+                    break;
             }
+            opcion = elegirOpcion();
+        }
+        System.out.println("Adios...");
     }
 
     public static boolean validarSismosVacio(double[] sismos){
         return sismos[0]>0;
     }
 
-    public static void arregloVacio(double[] sismos){
+    public static void caso2(double[] sismos){
+        if (validarSismosVacio(sismos)){
+            mostrarDouble(buscarMayorSismo(sismos));
+        }else{
+            System.out.println("No se registran datos de sismos (arreglo vacio");
+        }
+    }
 
+    public static void caso3(double[] sismos){
+        if (validarSismosVacio(sismos)){
+            mostrarInt(contarSismos(sismos,5.0));
+        }else{
+            System.out.println("No se registran datos de sismos (arreglo vacio");
+        }
+    }
+
+    public static void caso4(double[] sismos){
+        if (validarSismosVacio(sismos)){
+            enviarSMS(contarSismos(sismos,7.0));
+        }else{
+            System.out.println("No se registran datos de sismos (arreglo vacio");
+        }
     }
 
     public static double []crearArreglo(){
@@ -93,6 +112,7 @@ public class Taller01Sismos {
     }
 
     public static void mostrarSismos(double[] sismos) {
+        System.out.println("Sismos ingresados:");
         System.out.println(Arrays.toString(sismos));
     }
 
